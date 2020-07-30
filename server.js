b6762c912901d178ff5d3f8dd92892cdb08f086f
MDJasserHussain <jasserhussain@gmail.com>
2020-07-30 17:40:26 +0530

    small change

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 const mockEvents = {
     events: [
         { title: 'an event', id: 1, description: 'something really cool' },
-        { title: 'another event', id: 2, description: 'something even cooler' }
+        { title: 'adityas event', id: 2, description: 'something even cooler' }
     ]
 };
 
